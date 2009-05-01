747b246397c38ac08034d109357e33fdfe1a4e61
Diego Carrion <dc.rec1@gmail.com>
2009-05-01 19:54:29 -0300

    should_have_attached_file :avatar validates if a model has an avatar or not

--- lib/remarkable_paperclip.rb
+++ lib/remarkable_paperclip.rb
@@ -1,2 +1,31 @@
-module RemarkablePaperclip
+require 'remarkable'
+require 'remarkable/messages'
+require 'remarkable/dsl/assertions'
+require 'remarkable/matchers'
+require 'remarkable/macros'
+require 'remarkable/base'
+
+module Remarkable
+  module Paperclip
+    class Base < Remarkable::Base
+    end
+    module Matchers
+      class HaveAttachedFileMatcher < Remarkable::Paperclip::Base
+        def initialize(attribute)
+          @attribute = attribute.to_s
+        end
+        
+        def matches(model)
+          model.respond_to? "before_#{@attribute}_post_process_callback_chain"
+        end
+      end
+
+      def have_attached_file(attribute)
+        HaveAttachedFileMatcher.new(attribute).spec(self)
+      end
+
+    end
+  end
 end
+
+Remarkable.include_matchers!(Remarkable::Paperclip, Spec::Example::ExampleGroup)
--- spec/remarkable_paperclip_spec.rb
+++ spec/remarkable_paperclip_spec.rb
@@ -1,5 +1,26 @@
-describe "RemarkablePaperclip" do
-  it "should have real tests" do
-    false.should eql(true)
+require 'rubygems'
+require 'active_record'
+require 'paperclip'
+require 'lib/remarkable_paperclip'
+
+require 'ruby2ruby'
+
+
+class Person < ActiveRecord::Base
+  has_attached_file :avatar
+end
+
+class Monster < ActiveRecord::Base
+end
+
+describe Remarkable::Paperclip do
+  describe "have_attached_file" do
+    it "should validate that a model has an attached file" do
+      have_attached_file(:avatar).matches(Person).should be_true
+    end
+    
+    it "should validate that a model doenst has an attached file" do
+      have_attached_file(:avatar).matches(Monster).should be_false
+    end
   end
 end
