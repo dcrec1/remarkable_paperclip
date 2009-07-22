2eea51a10bddcd40548e6c81b16e857f45168ca6
Willian Molinari <wmolinari@ikwa.com.br>
2009-07-23 01:59:31 +0800

    Adding locales to translate test messages
    Signed-off-by: Diego Carrion <dc.rec1@gmail.com>

--- init.rb
+++ init.rb
@@ -2,6 +2,10 @@ if RAILS_ENV == "test"
 
   require 'remarkable_activerecord'
 
+  # Available Locales
+  Remarkable.add_locale RAILS_ROOT + "/vendor/plugins/remarkable_paperclip/locales/en.yml"
+  Remarkable.add_locale RAILS_ROOT + "/vendor/plugins/remarkable_paperclip/locales/pt-BR.yml"
+
   require File.join(File.dirname(__FILE__), "lib", "remarkable_paperclip")
 
   require 'spec'
